c2fab7d6411c48ea1eef79e7f9758da6c0b69dae
Rishi Puri <puririshi98@berkeley.edu>
2023-02-06 17:48:10 -0800

    Update make_timm_diff.py

--- make_timm_diff.py
+++ make_timm_diff.py
@@ -1,5 +1,15 @@
 import timm
 import subprocess
+def make_diff(x):
+	tru_dict = {}
+	false_dict = {}
+	for (model, amp, chanlast), sampersec in x.items():
+		if "True" in chanlast:
+			tru_dict[(model, amp)] = float(sampersec)
+		else:
+			false_dict[(model, amp)] = float(sampersec)
+	print("perf diff w/ channels last:", {key:(tru_dict[key]-false_dict[key])/false_dict[key] for key in tru_dict.keys()})
+
 samples_per_sec = {}
 for amp in [False, True]:
 	for model_name in timm.list_models():
@@ -20,7 +30,7 @@ for amp in [False, True]:
 			try:
 				samples_per_sec[key] = time
 				print('train_samples_per_sec:', time)
-				print('running_dict:', samples_per_sec)
+				#print('running_dict:', samples_per_sec)
 				del time
 			except:
 				contin = False
@@ -35,4 +45,4 @@ for amp in [False, True]:
 						print(line)
 					for line in str(err.decode()).splitlines():
 						print(line)
-print(samples_per_sec)
+		make_diff(samples_per_sec)
